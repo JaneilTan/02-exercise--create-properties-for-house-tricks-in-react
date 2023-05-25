7036a20a30bd2625d8fff63f586f05eacc05ed59
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-25 20:27:29 +1200

    imported useNavigate to redirect back to the homepage but caught an error

--- client/src/components/AddProperty.js
+++ client/src/components/AddProperty.js
@@ -1,5 +1,6 @@
 import "./AddProperty.css";
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const AddProperty = () => {
   const [title, setTitle] = useState('');
@@ -7,57 +8,76 @@ const AddProperty = () => {
   const [description, setDescription] = useState('');
   const [address, setAddress] = useState('');
   const [imgUrl, setImgUrl] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const property = { title, askingPrice, description, address, imgUrl };
+    const newProperty = { title, askingPrice, description, address, imgUrl };
 
-    fetch('http://localhost:5001/properties')
+    setIsPending(true);
+
+    fetch('http://localhost:5001/properties', {
+      method:'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProperty)
+    }).then(() => {
+      setIsPending(false);
+      
+    })
+
+    
   }
 
   return (
     <>
       <h1>Add a new property</h1>
       <form onSubmit={handleSubmit}>
-      <label>Title:</label>
+      <label htmlFor="Title">Title:</label>
       <input
         type="text"
+        id="Title"
         required
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
-      <label>Asking Price:</label>
+      <label htmlFor="Price">Asking Price:</label>
       <input
         type="text"
+        id="Price"
         required
         value={askingPrice}
         onChange={(event) => setAskingPrice(event.target.value)}
       />
-      <label>Description:</label>
+      <label htmlFor="Description">Description:</label>
       <input
         type="text"
+        id="Description"
         required
         value={description}
         onChange={(event) => setDescription(event.target.value)}
       />
-      <label>Address:</label>
+      <label htmlFor="Address">Address:</label>
       <input
         type="text"
+        id="Address"
         required
         value={address}
         onChange={(event) => setAddress(event.target.value)}
       />
-      <label>Image URL:</label>
+      <label htmlFor="ImgUrl">Image URL:</label>
       <input
         type="text"
+        id="ImgUrl"
         required
         value={imgUrl}
         onChange={(event) => setImgUrl(event.target.value)}
       />
-      <button>Submit</button>
+      { !isPending && <button onClick={() => navigate('/')}>Submit</button> }
+      { isPending && <button disabled>Submitting property...</button> }
       </form>
     </>
   );
-};
+}
 
 export default AddProperty;
